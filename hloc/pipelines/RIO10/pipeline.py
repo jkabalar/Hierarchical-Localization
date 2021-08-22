7ad35f93dd6b73b7ffdac4f5854044c54462705f
J Kabalar <32665838+jkabalar@users.noreply.github.com>
2021-08-22 22:45:04 +0200

    Create pipeline.py

---
+++ hloc/pipelines/RIO10/pipeline.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from ... import extract_features, match_features, reconstruction
+
+dataset = Path('datasets/RIO/')
+images = dataset / 'mapping/sensors/records_data/seq03_01/'
+
+outputs = Path('outputs/sfm/')
+sfm_pairs = outputs / 'pairs-exhaustive.txt'  # exhaustive matching
+sfm_dir = outputs / 'sfm_superpoint+superglue'
+
+feature_conf = extract_features.confs['superpoint_aachen']
+matcher_conf = match_features.confs['superglue']
+
+feature_path = extract_features.main(feature_conf, images, outputs)
+
+match_path = match_features.main(
+    matcher_conf, sfm_pairs, feature_conf['output'], outputs, exhaustive=True)
+
+reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, match_path)
