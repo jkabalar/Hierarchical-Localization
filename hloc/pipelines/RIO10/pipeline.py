e357eb5bf31fa4e94e31201ae6a5e719d1dbd272
J Kabalar <32665838+jkabalar@users.noreply.github.com>
2021-08-24 13:48:53 +0200

    Update pipeline.py

--- hloc/pipelines/RIO10/pipeline.py
+++ hloc/pipelines/RIO10/pipeline.py
@@ -2,19 +2,46 @@ from pathlib import Path
 
 from ... import extract_features, match_features, reconstruction
 
+
+
+#feature_path = Path(export_dir, feature_conf['output']+'.h5')
+#features = feature_conf['output']
+#match_path = Path(export_dir, f'{features}_{matcher_conf["output"]}_{sfm_pairs.stem}.h5')
+
 dataset = Path('datasets/RIO10')
 images = dataset / 'mapping/sensors/records_data/seq03_01'
 
+query_images = dataset / 'validation/sensors/records_data/seq03_01'
+query_list = dataset / 'retrieval.txt'
+
 outputs = Path('outputs/sfm/')
+results_path = outputs / 'RIO10SFM_hloc_superpoint+superglue_netvlad40.txt'  # the result file
+
 sfm_pairs = outputs / 'pairs-exhaustive.txt'  # exhaustive matching
 sfm_dir = outputs / 'sfm_superpoint+superglue'
+num_loc= 5
+loc_pairs = outputs / f'pairs-query-netvlad{num_loc}.txt'
 
 feature_conf = extract_features.confs['superpoint_inloc']
 matcher_conf = match_features.confs['superglue']
+retrieval_conf = extract_features.confs['netvlad']
 
-feature_path = extract_features.main(feature_conf, images, outputs)
+feature_path = extract_features.main(feature_conf, images, outputs, as_half=True)
 
 match_path = match_features.main(
-    matcher_conf, sfm_pairs, feature_conf['output'], outputs, exhaustive=True)
+    matcher_conf, sfm_pairs, feature_conf['output'], outputs)#, exhaustive=True)
 
 reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, match_path)
+
+global_descriptors = extract_features.main(retrieval_conf, images, outputs)
+global_descriptors = extract_features.main(retrieval_conf, query_images, outputs)
+
+pairs_from_retrieval.main(global_descriptors, loc_pairs, num_loc, query_list=query_list, db_model=sfm_dir)
+
+features = extract_features.main(
+        feature_conf, query_images, outputs, as_half=True)
+loc_matches = match_features.main(
+        matcher_conf, loc_pairs, feature_conf['output'], outputs)
+
+localize_sfm.main(
+    sfm_dir, query_list, sfm_pairs, features , loc_matches, results_path)
