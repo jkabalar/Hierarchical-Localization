2da6c10e0a2be0a6729a86a733338d247064f12a
jUlia Kabalar <julia.kabalar@gmail.com>
2021-08-25 07:02:38 +0000

    Check out pipeline

--- hloc/pipelines/RIO10/pipeline.py
+++ hloc/pipelines/RIO10/pipeline.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from ... import extract_features, match_features, reconstruction
+from ... import extract_features, match_features, reconstruction, pairs_from_retrieval
 
 
 
@@ -11,32 +11,33 @@ from ... import extract_features, match_features, reconstruction
 dataset = Path('datasets/RIO10')
 images = dataset / 'mapping/sensors/records_data/seq03_01'
 
-query_images = dataset / 'validation/sensors/records_data/seq03_01'
+query_images = dataset / 'validation/sensors/records_data/seq03_02'
 query_list = dataset / 'retrieval.txt'
 
 outputs = Path('outputs/sfm/')
 results_path = outputs / 'RIO10SFM_hloc_superpoint+superglue_netvlad40.txt'  # the result file
 
-sfm_pairs = outputs / 'pairs-exhaustive.txt'  # exhaustive matching
+sfm_pairs = outputs / 'pairs-exhaustive_try2.txt'  # exhaustive matching
 sfm_dir = outputs / 'sfm_superpoint+superglue'
-num_loc= 5
+num_loc= 1
 loc_pairs = outputs / f'pairs-query-netvlad{num_loc}.txt'
 
 feature_conf = extract_features.confs['superpoint_inloc']
-matcher_conf = match_features.confs['superglue']
+matcher_conf = match_features.confs['NN-superpoint']#['superglue']
 retrieval_conf = extract_features.confs['netvlad']
 
-feature_path = extract_features.main(feature_conf, images, outputs, as_half=True)
+#feature_path = extract_features.main(feature_conf, images, outputs, as_half=True)
+feature_path = Path(outputs, feature_conf['output']+'.h5')
+#match_path = match_features.main(
+#    matcher_conf, sfm_pairs, feature_conf['output'], outputs, exhaustive=True)
+features = feature_conf['output']
+match_path = Path(outputs, f'{features}_{matcher_conf["output"]}_{sfm_pairs.stem}.h5')
+#reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, match_path)
 
-match_path = match_features.main(
-    matcher_conf, sfm_pairs, feature_conf['output'], outputs)#, exhaustive=True)
-
-reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, match_path)
-
-global_descriptors = extract_features.main(retrieval_conf, images, outputs)
+#global_descriptors = extract_features.main(retrieval_conf, images, outputs)
 global_descriptors = extract_features.main(retrieval_conf, query_images, outputs)
 
-pairs_from_retrieval.main(global_descriptors, loc_pairs, num_loc, query_list=query_list, db_model=sfm_dir)
+#pairs_from_retrieval.main(global_descriptors, loc_pairs, num_loc, query_list=query_list, db_model=sfm_dir)
 
 features = extract_features.main(
         feature_conf, query_images, outputs, as_half=True)
