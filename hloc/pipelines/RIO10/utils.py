3712856bc899b393d202ea515b89cdd940b5b334
J Kabalar <32665838+jkabalar@users.noreply.github.com>
2021-09-10 16:47:58 +0200

    Update utils.py

--- hloc/pipelines/RIO10/utils.py
+++ hloc/pipelines/RIO10/utils.py
@@ -9,21 +9,21 @@ def generate_query_list(dataset_dir, path):
     model_name = 'PINHOLE'
     params = [float(i) for i in [fx, fy, cx, cy]]
     cam0 = Camera(id=0, model=model_name, width=int(width), height=int(height), params=params)
-    intrinsics = [cam0.model, cam0.width, cam0.height] + cam0.params
-    intrinsics = [str(p) for p in intrinsics]
+    #intrinsics = [cam0.model, cam0.width, cam0.height] + cam0.params
+    #intrinsics = [str(p) for p in intrinsics]
 
     queries = dataset / f'{slice_}/test-images-{slice_}.txt'
     with open(queries, 'r') as f:
         queries = [q.rstrip('\n') for q in f.readlines()]
     #RIO10/scene03/mapping/sensors/records_data/seq03_01
-    dataset_dir = "RIO10/scene03/validation/sensors/records_data/seq03_02/"
+    #dataset_dir = "RIO10/scene03/validation/sensors/records_data/seq03_02/"
     #frame-006270.color.jpg
     #data = map(lambda ts: ' '.join([f'frame/{ts}.png']+intrinsics), timestamps)
     data = []
     for frame_id in range(1000):
         frame_id.zfill(6)
         name = dataset_dir+ "frame-"+ str(frame_id)+".color.jpg"
-        intrinsics = [name, [cam0.model, cam0.width, cam0.height] + cam0.params
+        intrinsics = [name, [cam0.model, cam0.width, cam0.height] + cam0.params]
         intrinsics = [str(p) for p in intrinsics]
         data.append(' '.join(map(str, p)))
     with open(path, 'w') as f:
